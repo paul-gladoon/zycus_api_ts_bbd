c77d66c5d7f3d5f1357322f7c578ea511baf4cd4
Pavel Gladoon <pagl@ciklum.com>
2020-04-22 18:28:15 +0300

    remove_readme

--- eInvoice/implementation/auth/auth.ts
+++ eInvoice/implementation/auth/auth.ts
@@ -2,7 +2,6 @@ import 'codeceptjs'
 import {Microservice} from '../../microservice_objects'
 
 declare const Given: any
-
 const microservice = new Microservice()
 
 Given('I am send auth GET Request', async () => {
